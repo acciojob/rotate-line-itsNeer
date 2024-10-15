548ba263b112de058377f7e20f0c57455f22c9d2
Neeraj Shrivas <shrivasneeraj01@gmail.com>
2024-10-15 22:35:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let line = document.getElementById('line');
+
+let angle = 0;
+function rotateLine() {
+    angle += 2;
+    line.style.transform = `rotate(${angle}deg)`;
+}
+
+setInterval(rotateLine, 20);
